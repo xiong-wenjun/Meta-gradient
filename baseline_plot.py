b4b370c70a447fbe8ee038512773ca87e0939fd1
xiongwenjun <wenjunxiong006@gmail.com>
2025-11-21 14:50:19 +0800

    multi-actor with vtrace

--- baseline_plot.py
+++ baseline_plot.py
@@ -25,15 +25,14 @@ def plot_baseline(npy_file):
     plt.title("Baseline Performance ({} seeds)".format(data.shape[0]))
     plt.grid(True)
     plt.legend()
-    plt.savefig("baseline_curve.png", dpi=200)
+    plt.savefig("baseline_vtrace_curve.png", dpi=200)
     plt.show()
 
-    print("绘图完成，已保存为 baseline_curve.png")
-
+    print("绘图完成，已保存为 baseline_vtrace_curve.png")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--file", type=str, required=False, default="/gemini/code/project/2025_11_21_13_45_01.npy",
+    parser.add_argument("--file", type=str, required=False, default="/gemini/code/project/baseline_vtrace_multi_actor_2025_11_21_14_45_50.npy",
                         help="/gemini/code/project/2025_11_21_13_45_01.npy")
     args = parser.parse_args()
 
